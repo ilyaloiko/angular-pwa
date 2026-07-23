61fb78a427f616a5eb858526013fe0fb0c66c7d6
Ilya Loika <ilyaloiko@gmail.com>
2026-07-23 17:21:39 +0300

    dashboard improvements

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -11,17 +11,21 @@ import { CandidateLocalRepository } from '../../core/repositories/candidate-loca
   styleUrl: './dashboard.scss',
 })
 export class Dashboard implements OnInit {
-
-  all = signal(0);
+  total = signal(0);
   favorite = signal(0);
+  frontend = signal(0);
+  backend = signal(0);
+  fullstack = signal(0);
 
   constructor(private readonly candidateLocalRepository: CandidateLocalRepository) {}
 
   ngOnInit(): void {
-    this.candidateLocalRepository.getList()
-      .then(candidates => {
-        this.all.set(candidates.length);
-        this.favorite.set(candidates.filter(item => item.favorite).length);
-      });
+    this.candidateLocalRepository.getList().then((candidates) => {
+      this.total.set(candidates.length);
+      this.favorite.set(candidates.filter((item) => item.favorite).length);
+      this.frontend.set(candidates.filter((item) => item.position === 'Frontend').length);
+      this.backend.set(candidates.filter((item) => item.position === 'Backend').length);
+      this.fullstack.set(candidates.filter((item) => item.position === 'Full-Stack').length);
+    });
   }
 }
