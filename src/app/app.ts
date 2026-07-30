041d3e542c84469389c331bc9ff8d2c46d38d3b0
Ilya Loika <ilyaloiko@gmail.com>
2026-07-30 19:00:35 +0300

    renaming, fix build

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-pwa');
+  protected readonly title = signal('candidate-hub');
 }
